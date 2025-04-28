d2e4d85079f8bdb44d3044a14b22479456781c60
Pratikbarche <pratik.barche@gmail.com>
2025-04-28 15:46:25 +0530

    first commit

---
+++ client/build/precache-manifest.fedcd1964d2991341ca2987be62bdfbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bae2aff7d1312864bbcd6212eb0452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db0da0afa0a671521ea3",
+    "url": "/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0da0afa0a671521ea3",
+    "url": "/static/js/main.9a157a4b.chunk.js"
+  },
+  {
+    "revision": "00fc4afc96365658fc09",
+    "url": "/static/js/runtime-main.eda899bb.js"
+  }
+]);
